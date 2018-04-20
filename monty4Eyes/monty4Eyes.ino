c4d6c9d6acab1832bb99a60c5df18c6725e1365c
5shekel <yair99@gmail.com>
2018-04-20 10:42:40 +0200

    multi dim example

--- monty4Eyes/monty4Eyes.ino
+++ monty4Eyes/monty4Eyes.ino
@@ -1,7 +1,6 @@
 #include "LedControlMS.h"
 #include "frames.h"
 
-
 LedControl lc = LedControl(11, 13, 10, 4);
 
 unsigned long delaytime = 250;
@@ -15,34 +14,11 @@ void setup() {
   }
 }
 
-
-void writeWWWWWWWWOnMatrix1() {
-
-  for (int row = 0; row++; row < 8) {
-    lc.setRow(0, row, a1[row]);
-    lc.setRow(1, row, a2[row]);
-    lc.setRow(2, row, a3[row]);
-    lc.setRow(3, row, a4[row]);
-  }
-  delay(delaytime);
-
-  for (int row = 0; row++; row < 8) {
-    lc.setRow(0, row, b1[row]);
-    lc.setRow(1, row, b2[row]);
-    lc.setRow(2, row, b3[row]);
-    lc.setRow(3, row, b4[row]);
-  }
-  delay(delaytime);
-
-  for (int row = 0; row++; row < 8) {
-    lc.setRow(0, row, c1[row]);
-    lc.setRow(1, row, c2[row]);
-    lc.setRow(2, row, c3[row]);
-    lc.setRow(3, row, c4[row]);
-  }
-  delay(delaytime);
-
-}
 void loop() {
-  writeWWWWWWWWOnMatrix1();
+  //here we update the entire a1-a4 frame in one multi dimension array over the 4 display segments
+  for (int seg = 0 ; seg++; seg < 4) {
+    for (int row = 0; row++; row < 8)
+      lc.setRow(seg, row, a[row]);
+    delay(delaytime);
+  }
 }
