2c3aad6e7c1e3195e1e3680a1c63f376c47e6a9a
5shekel <yair99@gmail.com>
2018-04-23 00:31:03 +0200

    works example one frame

--- monty4Eyes/monty4Eyes.ino
+++ monty4Eyes/monty4Eyes.ino
@@ -1,12 +1,31 @@
 #include "LedControlMS.h"
 #include "frames.h"
+//#include <Keypad.h>
 
 LedControl lc = LedControl(11, 13, 10, 4);
 
+//Yair's delay:
 unsigned long delaytime = 250;
 
+//Tommy's delay:
+//int delaytime = 250;
+//unsigned long currenttime = 0;
+
+//keypad definitions
+/*
+  const byte ROWS = 4; //four rows
+  const byte COLS = 3; //three columns
+  char keys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+  };
+  byte rowPins[ROWS] = {8, 7, 6, 5};
+  byte colPins[COLS] = {4, 3, 2};
+*/
 void setup() {
-  for (int iii = 0; iii++; iii < 4) {
+  for (int iii = 0; iii < 4; iii++) {
     lc.shutdown(iii, false);
     lc.setIntensity(iii, 15);
     lc.clearDisplay(iii);
@@ -15,10 +34,13 @@ void setup() {
 }
 
 void loop() {
-  //here we update the entire a1-a4 frame in one multi dimension array over the 4 display segments
-  for (int seg = 0 ; seg++; seg < 4) {
-    for (int row = 0; row++; row < 8)
-      lc.setRow(seg, row, a[row]);
-    delay(delaytime);
+  //  if(millis() > currenttime + delaytime){
+  //        currenttime = millis();
+
+  //here we update the entire a1-a4 frame in one multi
+  // dimension array over the 4 display segments
+  for (int seg = 0 ; seg < 4; seg++) {
+    for (int row = 0; row < 8; row++ )
+      lc.setRow(seg, row, a0[seg][row]);
   }
 }
