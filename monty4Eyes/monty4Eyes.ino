27839c25f45c55954a0d505edce2b25c7056cec9
5shekel <yair99@gmail.com>
2018-04-23 13:51:39 +0200

    2 animations

--- monty4Eyes/monty4Eyes.ino
+++ monty4Eyes/monty4Eyes.ino
@@ -1,17 +1,19 @@
 #include "LedControlMS.h"
 #include "frame.h"
 
-//#include <Keypad.h>
+
 
 LedControl lc = LedControl(11, 13, 10, 4);
 
 //Tommy's delay:
 int delaytime = 50;
 unsigned long currenttime = 0;
-int frameTotal; //temp value for number of frames in a MMA (multi multi array)
+int frameTotal; //temp value for number of frames our 3D array
 
 //keypad definitions
 /*
+  #include <Keypad.h>
+
   const byte ROWS = 4; //four rows
   const byte COLS = 3; //three columns
   char keys[ROWS][COLS] = {
@@ -23,7 +25,10 @@ int frameTotal; //temp value for number of frames in a MMA (multi multi array)
   byte rowPins[ROWS] = {8, 7, 6, 5};
   byte colPins[COLS] = {4, 3, 2};
 */
+
 void setup() {
+  Serial.begin(115200);
+
   for (int iii = 0; iii < 4; iii++) {
     lc.shutdown(iii, false);
     lc.clearDisplay(iii);
@@ -31,7 +36,10 @@ void setup() {
   }
 }
 
+
 void loop() {
+
+
   frameTotal = (sizeof(c) / 32);
   for (int frame = 0; frame < frameTotal; frame++) {
     for (int seg = 0 ; seg < 4; seg++) {
@@ -40,4 +48,14 @@ void loop() {
     }
     delay(delaytime);
   }
+  delay(1000);
+  frameTotal = (sizeof(d) / 32);
+  for (int frame = 0; frame < frameTotal; frame++) {
+    for (int seg = 0 ; seg < 4; seg++) {
+      for (int row = 0; row < 8; row++ )
+        lc.setRow(seg, row, d[frame][seg][row]);
+    }
+    delay(delaytime);
+  }
+  delay(1000);
 }
