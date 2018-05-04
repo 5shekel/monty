b1fdcd46ad66ae48b20226b001125a5818702238
5shekel <yair99@gmail.com>
2018-05-04 18:12:53 +0200

    4x2

--- monty4Eyes/frame.h
+++ monty4Eyes/frame.h
@@ -1,13 +1,28 @@
-enum animationState {
-  Snums,
-  Sdance,
-  Ssnail,
+enum animations {
+  S0_probuzeni = 0,
+  S1_normal = 1,
+  S2_vesele_mrk = 2,
+  S3_do_rozzlobeni = 3,
+  S4_do_smutku = 4,
+  S5_do_spanku = 5,
+  S6_do_srdicka = 6,
+  S7_do_udiveni = 7,
+  S8_normal_default_possition = 8,
+  S9_sleeping_default_possition = 9,
   def = 99
 } animState;
 
 //this is the size of each animation, based on order in enum above
-int size_array[] = {0, 8, 22};
-int loop_array[] = {1, 3, 3};
-#include "data/animation_nums.h"
-#include "data/animation_dance.h"
-#include "data/animation_snail.h"
+int size_array[] = {7, 13, 11, 27, 26, 7, 19, 29, 1, 1};
+int loop_array[] = {1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3};
+
+#include "data/animation_0_probuzeni.h"
+#include "data/animation_1_normal.h"
+#include "data/animation_2_vesele_mrk.h"
+#include "data/animation_3_do_rozzlobeni.h"
+#include "data/animation_4_do_smutku.h"
+#include "data/animation_5_do_spanku.h"
+#include "data/animation_6_do_srdicka.h"
+#include "data/animation_7_do_udiveni.h"
+#include "data/normal_default_possition.h"
+#include "data/sleeping_default_possition.h"
--- monty4Eyes/monty4Eyes.ino
+++ monty4Eyes/monty4Eyes.ino
@@ -1,24 +1,20 @@
 
-
 // monty eye test
 /*
    = wiring ARDUNIO=
-
    MATRIX / SPI
    CLK = D13
    CS = D10
    DIN = D11
-
    TOUCH / I2C
    SCL = A5
    SDA = A4
-
 */
 #include <Adafruit_GFX.h> // uses the adafruit GFX lib
 #include "Max72xxPanel.h" // https://github.com/markruys/arduino-Max72xxPanel
 
-#define numberOfHorizontalDisplays  2
-#define numberOfVerticalDisplays    2
+#define numberOfHorizontalDisplays  4
+#define numberOfVerticalDisplays    4
 #define pinCS 10 // Attach CS to this pin, DIN to MOSI and CLK to SCK (cf http://arduino.cc/en/Reference/SPI )
 Max72xxPanel matrix = Max72xxPanel(10, numberOfHorizontalDisplays, numberOfVerticalDisplays);
 
@@ -58,13 +54,23 @@ void setup() {
   matrix.setPosition(2, 3, 0); // The third display is at <2, 0>
   matrix.setPosition(3, 2, 0); // And the last display is at <3, 0>
 
+  matrix.setPosition(4, 0, 0); // The first display is at <0, 0>
+  matrix.setPosition(5, 0, 0); // The second display is at <1, 0>
+  matrix.setPosition(6, 0, 0); // The third display is at <2, 0>
+  matrix.setPosition(7, 0, 0);
+
+
   matrix.setRotation(0, 3);
   matrix.setRotation(1, 1);
   matrix.setRotation(2, 1);
   matrix.setRotation(3, 3);
+  matrix.setRotation(4, 2);
+  matrix.setRotation(5, 2);
+  matrix.setRotation(6, 2);
+  matrix.setRotation(7, 2);
 
   matrix.fillScreen(LOW);
-  matrix.drawBitmap(0, 0, nums[0], 16, 16, 255);
+///  matrix.drawBitmap(0, 0, nums[0], 32, 16, 255);
   matrix.write();
 
   delay(500);
@@ -72,14 +78,13 @@ void setup() {
   matrix.write();
 
 }
-
 int getTouch() {
   currtouched = cap.touched();
 
   for (uint8_t i = 0; i < 12; i++) {
     // it if *is* touched and *wasnt* touched before, alert!
     if ((currtouched & _BV(i)) && !(lasttouched & _BV(i)) ) {
-      animState = animationState(i);
+      animState = i;
       step = 0;
       maxstep = size_array[i];
       loops = loop_array[i]; //how many loops to run the animation
@@ -103,25 +108,34 @@ void loop() {
 
     matrix.fillScreen(LOW);
 
-    if (animState == Snums) {
-      matrix.drawBitmap(0, 0, nums[0], 16, 16, 255);
-    } else if (animState == Sdance) {
-      matrix.drawBitmap(0, 0, dance[step], 16, 16, 255);
-    } else if (animState == Ssnail) {
-      matrix.drawBitmap(0, 0, snail[step], 16, 16, 255);
-    }
+if (animState == S8_normal_default_possition) {
+  matrix.drawBitmap(0, 0, normal_default_possition[step], 32, 16, 255);
+} else if (animState == S0_probuzeni) {
+  matrix.drawBitmap(0, 0, probuzeni[step], 32, 16, 255);
+} else if (animState == S1_normal) {
+  matrix.drawBitmap(0, 0, normal[step], 32, 16, 255);
+} else if (animState == S2_vesele_mrk) {
+  matrix.drawBitmap(0, 0, vesele_mrk[step], 32, 16, 255);
+} else if (animState == S3_do_rozzlobeni) {
+  matrix.drawBitmap(0, 0, do_rozzlobeni[step], 32, 16, 255);
+} else if (animState == S4_do_smutku) {
+  matrix.drawBitmap(0, 0, do_smutku[step], 32, 16, 255);
+} else if (animState == S5_do_spanku) {
+  matrix.drawBitmap(0, 0, do_spanku[step], 32, 16, 255);
+} else if (animState == S6_do_srdicka) {
+  matrix.drawBitmap(0, 0, do_srdicka[step], 32, 16, 255);
+} else if (animState == S7_do_udiveni) {
+  matrix.drawBitmap(0, 0, do_udiveni[step], 32, 16, 255);
+} else if (animState == S9_sleeping_default_possition) {
+  matrix.drawBitmap(0, 0, sleeping_default_possition[step], 32, 16, 255);
+}
 
-    if (step >= maxstep - 1) {
-      step = 0;
-      loopCount++;
-    }
-    if (loopCount >= loops) {
-      Serial.println("def");
-      loopCount=0;
+
+    if (step >= maxstep-1) {
       animState = def;
+      step = 0;
     }
     step++;
-    
     matrix.write();
   }
 }
